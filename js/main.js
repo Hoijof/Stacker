0c01c2de43bde8b491d5e728c4ee4f4268be3f22
Humberto Gomez <hoijof@gmail.com>
2017-03-07 22:00:31 +0100

    Indents

--- js/main.js
+++ js/main.js
@@ -1,57 +1,58 @@
 var todoManager = new TodoManager();
 
 
-document.addEventListener('DOMContentLoaded', function() {
-	"strict mode";
+document.addEventListener('DOMContentLoaded', function () {
+  "strict mode";
 
-	var input = document.querySelector("#formContainer input"),
+  var input = document.querySelector("#formContainer input"),
     mainContainer = $("#mainContainer");
 
-	input.addEventListener('keyup', function(event) {
-		event.stopPropagation();
-		var todo = "";
-		if (event.keyCode === 13) {
-			todo = todoManager.addTodo(new Todo(input.value));
-			todo.render();
-			this.value = "";
-			todoManager.saveTodos();
-		}		
-	});
+  input.addEventListener('keyup', function (event) {
+    event.stopPropagation();
+    var todo = "";
+    if (event.keyCode === 13) {
+      todo = todoManager.addTodo(new Todo(input.value));
+      todo.render();
+      this.value = "";
+      todoManager.saveTodos();
+    }
+  });
 
-  mainContainer.on("dblclick", "div", function() {
-		var elem = this.querySelector('.cardText');
+  mainContainer.on("dblclick", "div", function () {
+    var elem = this.querySelector('.cardText');
 
-		console.log(elem.innerHTML);
-	});
 
-  mainContainer.on("click", "div > div", function() {
-		var option = $(this).html(),
-			todoId = parseInt(this.parentElement.id.split("_")[1]),
-			depth = 0;
+    console.log(elem.innerHTML);
+  });
 
-		switch (option) {
-			case '+':
-				depth = parseInt(this.parentElement.style.zIndex) + 1;
+  mainContainer.on("click", "div > div", function () {
+    var option = $(this).html(),
+      todoId = parseInt(this.parentElement.id.split("_")[1]),
+      depth = 0;
 
-				this.parentElement.style.zIndex = depth;
-				todoManager.todos[todoId].depth = depth;
-				this.parentElement.getElementsByClassName("depth")[0].innerHTML = depth;
-			break;
-			case '-':
-				depth = parseInt(this.parentElement.style.zIndex) - 1;
+    switch (option) {
+      case '+':
+        depth = parseInt(this.parentElement.style.zIndex) + 1;
 
-				this.parentElement.style.zIndex = depth;
-				todoManager.todos[todoId].depth = depth;
-				this.parentElement.getElementsByClassName("depth")[0].innerHTML = depth;
-			break;
-			case 'x':
-				todoManager.removeTodo(todoId, -1);
-			break;
-		}
-		todoManager.saveTodos();
-	});
+        this.parentElement.style.zIndex = depth;
+        todoManager.todos[todoId].depth = depth;
+        this.parentElement.getElementsByClassName("depth")[0].innerHTML = depth;
+        break;
+      case '-':
+        depth = parseInt(this.parentElement.style.zIndex) - 1;
 
-	todoManager.loadTodos();
-	todoManager.renderAllTodos();
-	input.focus();
+        this.parentElement.style.zIndex = depth;
+        todoManager.todos[todoId].depth = depth;
+        this.parentElement.getElementsByClassName("depth")[0].innerHTML = depth;
+        break;
+      case 'x':
+        todoManager.removeTodo(todoId, -1);
+        break;
+    }
+    todoManager.saveTodos();
+  });
+
+  todoManager.loadTodos();
+  todoManager.renderAllTodos();
+  input.focus();
 });
--- js/prototypes.js
+++ js/prototypes.js
@@ -1,146 +1,149 @@
+function createDiv(text, className) {
+  var div = document.createElement('div');
+  div.innerHTML = text;
+  div.style.float = "right";
+  div.style.cursor = "pointer";
+  div.style.width = "16px";
+  div.style.textAlign = "center";
 
-function createDiv (text, className) {
-	var div = document.createElement('div');
-	div.innerHTML = text;
-	div.style.float = "right";
-	div.style.cursor = "pointer";
-	div.style.width = "16px";
-	div.style.textAlign = "center";
+  if (className !== undefined) {
+    div.className = className;
+  }
 
-	if (className !== undefined) {
-		div.className = className;
-	}
-
-	return div;
+  return div;
 }
 // PROTOTYPES
 
 /*
  * Returns the size of an array
-*/
-Array.prototype.size = function(){ 
-	return this.filter(function(a){return a !== undefined;}).length
+ */
+Array.prototype.size = function () {
+  return this.filter(function (a) {
+    return a !== undefined;
+  }).length
 };
 
 /*
  * Returns the size of an object
-*/
-Object.size = function(obj) {
-    var size = 0, key = "";
+ */
+Object.size = function (obj) {
+  var size = 0, key = "";
 
-    for (key in obj) {
-        if (obj.hasOwnProperty(key)) size++;
-    }
-    return size;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
 };
 
 
 function TodoManager() {
-	this.todos = [];
+  this.todos = [];
 }
 
-TodoManager.prototype.addTodo = function(todo) {
-	this.todos.push(todo);
-	todo.id = this.todos.length - 1;
+TodoManager.prototype.addTodo = function (todo) {
+  this.todos.push(todo);
+  todo.id = this.todos.length - 1;
 
-	return todo;
+  return todo;
 };
 
-TodoManager.prototype.removeTodo = function(id) {
-	this.todos[id].derender();
-	this.todos[id] = undefined;
+TodoManager.prototype.removeTodo = function (id) {
+  this.todos[id].derender();
+  this.todos[id] = undefined;
 }
 
-TodoManager.prototype.redrawTodo = function(todoId, x, y) {
-	var todo = this.todos[todoId];
-	todo.x = x;
-	todo.y = y;
+TodoManager.prototype.redrawTodo = function (todoId, x, y) {
+  var todo = this.todos[todoId];
+  todo.x = x;
+  todo.y = y;
 
-	todo.derender();
-	todo.render();
+  todo.derender();
+  todo.render();
 };
 
-TodoManager.prototype.saveTodos = function() {
-	if(typeof(Storage) !== "undefined") {
-    	localStorage.setItem("todos", JSON.stringify(this.todos));
-	} else {
-	    console.log("you don't have local storage :(");
-	}
+TodoManager.prototype.saveTodos = function () {
+  if (typeof(Storage) !== "undefined") {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  } else {
+    console.log("you don't have local storage :(");
+  }
 };
 
-TodoManager.prototype.loadTodos = function() {
-	var i;
+TodoManager.prototype.loadTodos = function () {
+  var i;
 
-	this.todos = JSON.parse(localStorage.getItem("todos"));
-	if (this.todos == null) this.todos = [];
+  this.todos = JSON.parse(localStorage.getItem("todos"));
+  if (this.todos == null) this.todos = [];
 
-	this.clearArray();
+  this.clearArray();
 };
 
-TodoManager.prototype.clearArray = function() {
-	var i;
+TodoManager.prototype.clearArray = function () {
+  var i;
 
-	this.todos = this.todos.filter(function(elem){return elem !== undefined && elem !== null});
-	for (i in this.todos) {
-		if (this.todos.hasOwnProperty(i)) {
-			this.todos[i].id = i;
-		}
-	}
+  this.todos = this.todos.filter(function (elem) {
+    return elem !== undefined && elem !== null
+  });
+  for (i in this.todos) {
+    if (this.todos.hasOwnProperty(i)) {
+      this.todos[i].id = i;
+    }
+  }
 };
 
-TodoManager.prototype.renderAllTodos = function() {
-	this.todos.forEach(function(todo) {
-		todo.__proto__ = Todo.prototype;
-		todo.render();
-	})
+TodoManager.prototype.renderAllTodos = function () {
+  this.todos.forEach(function (todo) {
+    todo.__proto__ = Todo.prototype;
+    todo.render();
+  })
 };
 
 function Todo(name) {
-	this.id = -1;
-	this.name = name;
-	this.depth = 100;
-	this.x = 100;
-	this.y = 100;
-	this.color = '#'+Math.floor(Math.random()*16777215).toString(16);
+  this.id = -1;
+  this.name = name;
+  this.depth = 100;
+  this.x = 100;
+  this.y = 100;
+  this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-Todo.prototype.render = function() {
-	var node = document.createElement('div'),
-		text = document.createElement('div'),
-		that = this;
+Todo.prototype.render = function () {
+  var node = document.createElement('div'),
+    text = document.createElement('div'),
+    that = this;
 
- 	text.innerHTML = this.name
- 	text.className = "cardText";
- 	
-	node.id = "todo_" + this.id;
-	node.className = "card card-1";
-	node.style.position = "fixed";
-	node.style.left = this.x;
-	node.style.top = this.y;
-	node.style.cursor = "-webkit-grab";
-	node.style.zIndex = this.depth;
+  text.innerHTML = this.name
+  text.className = "cardText";
 
-	document.getElementById("mainContainer").appendChild(node);
+  node.id = "todo_" + this.id;
+  node.className = "card card-1";
+  node.style.position = "fixed";
+  node.style.left = this.x;
+  node.style.top = this.y;
+  node.style.cursor = "-webkit-grab";
+  node.style.zIndex = this.depth;
 
-	node.appendChild(createDiv('x', "controll"));
-	node.appendChild(createDiv('-', "controll"));
-	node.appendChild(createDiv('+', "controll"));
-	node.appendChild(createDiv(this.depth, "depth"));
+  document.getElementById("mainContainer").appendChild(node);
 
-	node.appendChild(text);
+  node.appendChild(createDiv('x', "controll"));
+  node.appendChild(createDiv('-', "controll"));
+  node.appendChild(createDiv('+', "controll"));
+  node.appendChild(createDiv(this.depth, "depth"));
 
-	$("#todo_" + this.id).draggable({
-		stop: function() {
-			var todo = $("#todo_" + that.id)
-			that.x = todo.css("left");
-			that.y = todo.css("top");
+  node.appendChild(text);
 
-			todoManager.saveTodos();
-		}
-	});
+  $("#todo_" + this.id).draggable({
+    stop: function () {
+      var todo = $("#todo_" + that.id)
+      that.x = todo.css("left");
+      that.y = todo.css("top");
+
+      todoManager.saveTodos();
+    }
+  });
 };
 
-Todo.prototype.derender = function() {
-	var node = document.getElementById("todo_" + this.id);
-	document.getElementById("mainContainer").removeChild(node);
+Todo.prototype.derender = function () {
+  var node = document.getElementById("todo_" + this.id);
+  document.getElementById("mainContainer").removeChild(node);
 };
